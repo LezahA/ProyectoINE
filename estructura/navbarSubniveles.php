f756046d6c4d2c5b085863274c8dabe4be88a7bf
Hazel Azucena Calderón Bonilla <cb22014@ues.edu.sv>
2025-06-22 16:34:19 -0600

    responsividad arreglada

--- estructura/navbarSubniveles.php
+++ estructura/navbarSubniveles.php
@@ -1,17 +1,14 @@
-<nav class="navbar navbar-expand-lg" style="background-color: #c4a371; border-bottom: 1px solid #ccc;">
+<nav class="navbar navbar-expand-md" style="background-color: #c4a371; border-bottom: 1px solid #ccc;">
     <div class="container-fluid">
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" 
-        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" 
-        aria-expanded="false" aria-label="Toggle navigation">
+    <a class="navbar-brand" href="../../index.php">
+        <img src="../../recursos/logo.svg" alt="Logo" style="width: 30px; height: 30px;">
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
         <span class="navbar-toggler-icon"></span>
     </button>
+    &nbsp
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-            <a class="nav-link" href="../../index.php" title="Inicio">
-            <img src="../../recursos/logo.svg" alt="Logo" style="width: 30px; height: 30px;">
-            </a>
-        </li>
+        <ul class="navbar-nav">
         <li class="nav-item">
             <a class="nav-link" href="../../Calculos/costoAnual.php" style="color: #fff; font-size: 1.2rem;">Costo anual</a>
         </li>
--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html lang="es">
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Proyecto Ingenieria de Negocios</title>
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
@@ -14,20 +15,18 @@
   <body>   
     <?php include './estructura/cabecera.php'; ?>
 
-    <nav class="navbar navbar-expand-lg" style="background-color: #c4a371; border-bottom: 1px solid #ccc;">
+    <nav class="navbar navbar-expand-md" style="background-color: #c4a371; border-bottom: 1px solid #ccc;">
       <div class="container-fluid">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" 
-          data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" 
-          aria-expanded="false" aria-label="Toggle navigation">
+        <a class="navbar-brand" href="./index.php">
+          <img src="./recursos/logo.svg" alt="Logo" style="width: 30px; height: 30px;">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
           <span class="navbar-toggler-icon"></span>
         </button>
+        &nbsp&nbsp&nbsp&nbsp&nbsp
+
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link" href="./index.php" title="Inicio">
-                <img src="./recursos/logo.svg" alt="Logo" style="width: 30px; height: 30px;">
-              </a>
-            </li>
+          <ul class="navbar-nav">
             <li class="nav-item">
               <a class="nav-link" href="./Calculos/costoAnual.php" style="color: #fff; font-size: 1.2rem;">Costo anual</a>
             </li>
@@ -36,8 +35,13 @@
       </div>
     </nav>
 
-    <video autoplay loop muted playsinline id="video-fondo">
-      <source src="./recursos/video1.mp4" type="video/mp4">
+    <div class="video-container">
+        <video autoplay loop muted playsinline id="video-fondo">
+            <source src="./recursos/video1.mp4" type="video/mp4">
+        </video>
+    </div>
+    <video autoplay loop muted playsinline id="video-fondo-movil" class="d-block d-md-none">
+      <source src="./recursos/video2.mp4" type="video/mp4">
     </video>
     <div class="main-content container-fluid">
     </div>
