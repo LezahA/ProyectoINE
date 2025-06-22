db0e283d33fc8aafbde15356bbbdd6ad6d7eb015
Hazel Azucena Calderón Bonilla <cb22014@ues.edu.sv>
2025-06-22 11:57:09 -0600

    cambios navbars

--- estructura/navbar.php
+++ estructura/navbar.php
@@ -1,17 +1,21 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-<div class="container-fluid">
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
+<nav class="navbar navbar-expand-lg" style="background-color: #c4a371; border-bottom: 1px solid #ccc;">
+    <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" 
+        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" 
+        aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-        <a class="nav-link" href="../index.php">Inicio</a>
+            <a class="nav-link" href="../index.php" title="Inicio">
+            <img src="../recursos/logo.svg" alt="Logo" style="width: 30px; height: 30px;">
+            </a>
         </li>
         <li class="nav-item">
-        <a class="nav-link" href="../Calculos/costoAnual.php">Costo anual</a>
+            <a class="nav-link" href="../Calculos/costoAnual.php" style="color: #fff; font-size: 1.2rem;">Costo anual</a>
         </li>
-    </ul>
+        </ul>
     </div>
-</div>
+    </div>
 </nav>
--- index.php
+++ index.php
@@ -9,27 +9,33 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <link rel="stylesheet" type="text/css" href="./estilos.css">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,600,700&display=swap" rel="stylesheet">
-
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
   </head>
   <body>   
     <?php include './estructura/cabecera.php'; ?>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+    <nav class="navbar navbar-expand-lg" style="background-color: #c4a371; border-bottom: 1px solid #ccc;">
       <div class="container-fluid">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" 
+          data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" 
+          aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-              <a class="nav-link" href="./index.php">Inicio</a>
-              </li>
-              <li class="nav-item">
-              <a class="nav-link" href="./Calculos/costoAnual.php">Costo anual</a>
-              </li>
+            <li class="nav-item">
+              <a class="nav-link" href="./index.php" title="Inicio">
+                <img src="./recursos/logo.svg" alt="Logo" style="width: 30px; height: 30px;">
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="./Calculos/costoAnual.php" style="color: #fff; font-size: 1.2rem;">Costo anual</a>
+            </li>
           </ul>
         </div>
       </div>
     </nav>
+
     <video autoplay loop muted playsinline id="video-fondo">
       <source src="./recursos/video1.mp4" type="video/mp4">
     </video>
