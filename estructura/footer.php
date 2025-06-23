847dd505a621c8eac01d4f032a7a5adf05c71d65
Douglas Barrera <53149834+Doisaac@users.noreply.github.com>
2025-06-23 17:58:54 -0600

    agregar tildes

--- estructura/footer.php
+++ estructura/footer.php
@@ -1,3 +1,3 @@
 <footer class="bg-dark text-white p-3">
-  <p class="text-center">Proyecto Ingenieria de Negocios &copy; 2025</p>
+  <p class="text-center">Proyecto Ingeniería de Negocios &copy; 2025</p>
 </footer>
